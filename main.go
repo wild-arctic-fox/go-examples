ea7c511c7d19514fc18376f73d1752deb6278444
Olena Veprytska <amusing.arctic.fox@gmail.com>
2023-09-06 12:40:13 +0300

    file write with go routines example

--- main.go
+++ main.go
@@ -2,110 +2,121 @@ package main
 
 import (
 	"fmt"
-	"go-booking-app/user"
-	"strings"
+	"math/rand"
+	"os"
+	"sync"
 	"time"
 )
 
-func Leave(student user.Student) {
-	student.LeaveUni()
+var actions = []string{
+	"log in",
+	"log out",
+	"create",
+	"read",
+	"update",
+	"delete",
+}
+
+type logItem struct {
+	action    string
+	timestamp time.Time
+}
+
+type User struct {
+	id    int
+	email string
+	logs  []logItem
+}
+
+func (u User) getactivityInfo() string {
+	result := fmt.Sprintf("ID: %d | Email: %s\nActivity Logs:\n", u.id, u.email)
+	for _, log := range u.logs {
+		result += log.action + " " + log.timestamp.String() + "\n"
+	}
+	return result
+}
+
+func randomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-+=)(*&^%$#@!"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+func generateUsers(amount int) []User {
+	users := make([]User, amount)
+	for i := 0; i < amount; i++ {
+		tmpUser := User{
+			id:    i,
+			email: fmt.Sprintf("%s@%s", randomString(10), "gmail.com"),
+			logs: []logItem{
+				{action: actions[rand.Intn(len(actions))], timestamp: time.Now()},
+				{action: actions[rand.Intn(len(actions))], timestamp: time.Now().Add(time.Hour)},
+				{action: actions[rand.Intn(len(actions))], timestamp: time.Now().Add(time.Minute)},
+			},
+		}
+		users[i] = tmpUser
+	}
+	return users
+}
+
+func safeUserInfo(user User, wg *sync.WaitGroup) error {
+	printlnRed(fmt.Sprintf("Write to file %d", user.id))
+	time.Sleep(time.Second)
+	filename := fmt.Sprintf("logs/uid_%d.txt", user.id)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString(user.getactivityInfo())
+	if err != nil {
+		return err
+	}
+
+	// done 1 task
+	wg.Done()
+	return nil
 }
 
 func main() {
 	println("Wubba lubba dub dub!")
 
+	// package that is used to wait for a collection of goroutines (concurrently executing functions)
+	// to finish their work before proceeding further in the program
+	wg := &sync.WaitGroup{}
+
+	users := generateUsers(60)
+
+	t := time.Now()
+
+	for _, user := range users {
+		// add 1 task
+		wg.Add(1)
+		go safeUserInfo(user, wg)
+
+	}
+
+	wg.Wait()
+
+	printlnYellow(fmt.Sprintf("Time Elapsed:", time.Since(t)))
+
 	// ///////////////////////////
-	/* STRUCTS
-	Custome complex types (class alternatives)
-	could have fields, methods
+	/* GO ROUTINES
+	lightweight threads
 	*/
 
-	// user1 := user.User{"Rick", 67, 'A', true, 66_000.90}
-	user2 := user.UserConstructor("Morty", 14, 'B', true, 800.00)
+	go printlnBlue("Concurrent Blue") // add to queue (and wait for blockers)
+	go printlnMagenta("Concurrent Magenta")
+	go printlnCyan("Concurrent Cyan")
 
-	// fmt.Println(user1)
-	user2.UpdateUser(255)
-	user2.PrintUser()
-	fmt.Println(user2.Aage.IsAdult())
+	// block execution
+	time.Sleep(10000)
 
-	Leave(user2)
-
-	fmt.Println(getTypeName(42))      // Output: This is an integer
-	fmt.Println(getTypeName(3.14))    // Output: This is a float
-	fmt.Println(getTypeName("Hello")) // Output: This is a string
-	fmt.Println(getTypeName(true))    // Output: Unknown type
-
-	// Time
-
-	fmt.Println(time.Now())
-
-	currentTime := time.Now()
-	formattedTime := currentTime.Format("2006-01-02 15:04:05")
-	printlnCyan(formattedTime)
-
-	arrInt := []int{78, 90, 67}
-	fmt.Println(sum(arrInt))
-
-	arrFloat32 := []float32{78.00, 90.00, 67.0109}
-	printlnYellow((sum(arrFloat32)))
-
-	fmt.Println(find(arrInt, 90))
-	fmt.Println(find(arrFloat32, 67.0109))
-	printlnGreen(find(arrFloat32, 67.0101))
-
-}
-
-// comparable - every type exept of map|slice or stuct that contains map|slice
-func find[C comparable](arr []C, el C) bool {
-	res := false
-	for _, v := range arr {
-		if v == el {
-			res = true
-		}
-	}
-	return res
-}
-
-func sum[number int | float32](num []number) number {
-	var sum number
-	for _, v := range num {
-		sum += v
-	}
-	return sum
-}
-
-// type switch
-
-func getTypeName(x interface{}) string {
-	switch x.(type) {
-	case int:
-		return "This is an integer"
-	case float64:
-		return "This is a float"
-	case string:
-		return "This is a string"
-	default:
-		return "Unknown type"
-	}
-}
-
-func handlePanic() {
-	// recover() built-in Go function used to handle panics
-	// r != nil => it means a panic occurred and was successfully recovered
-	r := recover()
-	if r != nil {
-		printlnCyan("Ignore panic")
-	}
-}
-
-func toUpperCase(inputString string) {
-	inputString = strings.ToUpper(inputString)
-	printlnCyan(inputString)
-}
-
-func toUpperCasePointer(inputString *string) {
-	*inputString = strings.ToUpper(*inputString)
-	printlnRed(*inputString)
+	printlnGreen("Non concurrent Green")
 }
 
 func printlnCyan[A any](inputString A) {
@@ -135,45 +146,3 @@ func printlnRed[A any](inputString A) {
 func printlnBlack[A any](inputString A) {
 	println(fmt.Sprintf("\x1b[%dm%s\x1b[0m", 30, fmt.Sprintf("%v", inputString)))
 }
-
-// func roundInc() func() uint8 {
-// 	var counter uint8 = 0
-// 	return func() uint8 {
-// 		counter++
-// 		return counter
-// 	}
-
-// }
-
-// func addSubInt8(a int8, b int8) (int8, int8, error) {
-// 	return a + b, a - b, errors.New("test error")
-// }
-
-// func switchCaseExample(a uint8) {
-// 	switch a {
-// 	case 1:
-// 		fmt.Println("one")
-// 	case 2:
-// 		fmt.Println("two")
-// 	case 3:
-// 		fmt.Println("three")
-// 	default:
-// 		fmt.Println("non 1, 2, 3")
-// 	}
-// }
-
-// func findMin(numbers ...uint) (uint, error) {
-
-// 	if len(numbers) == 0 {
-// 		return 0, errors.New("no numbers provided")
-// 	}
-// 	min := numbers[0]
-
-// 	for _, i := range numbers {
-// 		if min > i {
-// 			min = i
-// 		}
-// 	}
-// 	return min, nil
-
-// }
